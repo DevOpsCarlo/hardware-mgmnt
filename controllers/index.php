91f3afd52df3b29961e8930629b01eeeebf9d209
DevOpsCarlo <cpa.aviso@gmail.com>
2025-05-13 11:46:07 +0800

    Setup php views and controllers

---
+++ controllers/index.php
@@ -0,0 +1,21 @@
+<?php require("views/partials/head.php"); ?>
+
+
+<main class="container">
+  <div class="logo-container">
+    <img src="assets/image/logo.png" alt="TopRank Logo" class="toprank-image">
+  </div>
+  <div>
+    <h2 class="login-title">Login</h2>
+    <form action="">
+      <div class="input-container">
+        <input type="text" name="username">
+        <input type="password" name="password">
+      </div>
+      <button class="btn btn-submit">Submit</button>
+    </form>
+  </div>
+</main>
+
+
+<?php require("views/partials/footer.php"); ?>
